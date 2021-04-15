4ad95cfe49669479b63c355ce09e87aaf6a41244
DONG WOO SOHN <samsohn0128@gmail.com>
2021-04-15 14:50:01 +0900

    20210415

--- Solution.java
+++ Solution.java
@@ -1,11 +1,11 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static int T;
-	static int ans;
+	static int T, ans;
 
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("sample_input.txt"));
