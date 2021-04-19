7c2375d2e46bc0a484cf62801626d2952e3751f5
DONG WOO SOHN <samsohn0128@gmail.com>
2021-04-19 11:02:59 +0900

    20210419

--- Test.java
+++ Test.java
@@ -1,20 +1,6 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Test {
 	public static void main(String[] args) throws Exception {
-		Map<String, Integer> m = new HashMap<>();
-		String push = "asdf";
-		m.put(push, 1);
-		System.out.println(m.size());
-		m.forEach((k,v)->{
-			System.out.println("key : "+k +" value : "+v);
-		});
-		String push2 = "asdf";
-		m.put(push2, 2);
-		System.out.println(m.size());
-		m.forEach((k,v)->{
-			System.out.println("key : "+k +" value : "+v);
-		});
+		int mask = 12;
+		System.out.println(mask & 1<<4);
 	}
 }
