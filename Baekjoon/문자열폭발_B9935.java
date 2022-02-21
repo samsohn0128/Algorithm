143f4f74363ea39315931f20e02d93f057bc8954
손동우 <ssafy_coach_67@ssafy.com>
2022-02-21 10:07:29 +0900

    20220220

---
+++ Baekjoon/문자열폭발_B9935.java
@@ -0,0 +1,44 @@
+package com.company;
+
+import java.io.*;
+import java.util.Stack;
+
+public class 문자열폭발_B9935 {
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String strInput = br.readLine();
+        String bomb = br.readLine();
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < strInput.length(); i++) {
+            stack.push(strInput.charAt(i));
+            if (stack.size() >= bomb.length()) {
+                if (stack.peek() == bomb.charAt(bomb.length() - 1)) {
+                    boolean same = true;
+                    for (int j = 0; j < bomb.length(); j++) {
+                        if (stack.get(stack.size() - 1 - j) != bomb.charAt(bomb.length() - 1 - j)) {
+                            same = false;
+                            break;
+                        }
+                    }
+                    if (same) {
+                        for (int j = 0; j < bomb.length(); j++) {
+                            stack.pop();
+                        }
+                    }
+                }
+            }
+        }
+        if (stack.isEmpty())
+            System.out.println("FRULA");
+        else {
+            BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+            for (int i = 0; i < stack.size(); i++) {
+                bw.write(stack.get(i));
+            }
+            bw.flush();
+            bw.close();
+        }
+    }
+}
---
+++ Baekjoon/접미사배열_B11656.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class 접미사배열_B11656 {
+
+    public static void main(String[] args) throws Exception {
+        System.setIn(new FileInputStream("input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String strInput = br.readLine();
+        IntStream.range(0, strInput.length()).mapToObj(i -> strInput.substring(i)).sorted().collect(Collectors.toList()).forEach(System.out::println);
+    }
+}
