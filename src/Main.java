ee29d267f1749392695ba5a804d8eb2c99d9e8e6
손동우 <samsohn0128@gmail.com>
2022-09-24 00:11:42 +0900

    20220924

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,12 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
-
-    private static List<Integer> numberList = new ArrayList<>();
-    private static Tree tree;
-
-    private static StringBuilder answerBuilder = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = setBufferedReader();
         init(br);
-        String answer = solution();
-        System.out.println(answer);
         br.close();
     }
 
@@ -25,83 +16,5 @@ public class Main {
     }
 
     private static void init(BufferedReader br) throws Exception {
-        while (true) {
-            String input = br.readLine();
-            if (input == null) break;
-            numberList.add(Integer.parseInt(input));
-        }
-    }
-
-    private static String solution() {
-        tree = new Tree(new Node(numberList.get(0)));
-        for (int i = 1; i < numberList.size(); i++) {
-            tree.add(new Node(numberList.get(i)));
-        }
-        postTraversal(tree.root);
-
-        return answerBuilder.toString();
-    }
-
-    private static Node getParentNode(Node beforeNode, Node curNode) {
-        while (beforeNode.parent != null && beforeNode.parent.number < curNode.number) {
-            beforeNode = beforeNode.parent;
-        }
-        return beforeNode;
-    }
-
-    private static void postTraversal(Node node) {
-        if (node.lchild != null) {
-            postTraversal(node.lchild);
-        }
-        if (node.rchild != null) {
-            postTraversal(node.rchild);
-        }
-        makeAnswer(node);
-    }
-
-    private static void makeAnswer(Node node) {
-        answerBuilder.append(node.number);
-        answerBuilder.append('\n');
-    }
-
-    private static class Node {
-
-        private int number;
-        private Node parent;
-        private Node lchild;
-        private Node rchild;
-
-        public Node(int number) {
-            this.number = number;
-        }
-    }
-
-    private static class Tree {
-        private Node root;
-
-        public Tree(Node root) {
-            this.root = root;
-        }
-
-        private void add(Node node) {
-            Node parentNode = root;
-            while (true) {
-                if (parentNode.number < node.number) {
-                    if (parentNode.rchild != null) {
-                        parentNode = parentNode.rchild;
-                    } else {
-                        parentNode.rchild = node;
-                        break;
-                    }
-                } else {
-                    if (parentNode.lchild != null) {
-                        parentNode = parentNode.lchild;
-                    } else {
-                        parentNode.lchild = node;
-                        break;
-                    }
-                }
-            }
-        }
     }
 }
