2d32024a88d3d9ef9a5dd4c134e8c1b40a773c89
samsohn0128 <samsohn0128@gmail.com>
2023-06-01 16:57:59 +0900

    20230601

--- Problems/Baekjoon/모비스_B28074.java
+++ Problems/Baekjoon/모비스_B28074.java
@@ -2,7 +2,9 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 
-public class Main {
+public class 모비스_B28074 {
+    private static String input;
+
     public static void main(String[] args) throws Exception {
         init();
         String answer = solution();
@@ -12,10 +14,19 @@ public class Main {
     private static void init() throws Exception {
         System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        input = br.readLine();
     }
 
     private static String solution() {
-        String answer = "";
-        return answer;
+        String mobis = "MOBIS";
+        boolean[] visited = new boolean[26];
+        for (int i = 0; i < input.length(); i++) {
+            visited[input.charAt(i) - 'A'] = true;
+        }
+        boolean ableToMake = true;
+        for (int i = 0; i < mobis.length(); i++) {
+            ableToMake &= visited[mobis.charAt(i) - 'A'];
+        }
+        return ableToMake ? "YES" : "NO";
     }
 }
