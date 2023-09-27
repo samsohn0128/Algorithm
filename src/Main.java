445dc1e5bc2ec67992c122a3b175a0520d3cae72
samsohn0128 <samsohn0128@gmail.com>
2023-09-27 14:01:11 +0900

    20230927

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,19 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 
 public class Main {
+
     public static void main(String[] args) throws Exception {
         init();
-        int answer = solution();
+        long answer = solution();
         System.out.println(answer);
     }
 
     private static void init() throws Exception {
         System.setIn(new FileInputStream("input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
     }
 
-    private static int solution() {
+    private static long solution() {
         return 0;
     }
 }
