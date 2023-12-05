b4d53d847257083ea8c82efccdab31d7d51b6b2f
DongwooSohn <samsohn0128@gmail.com>
2023-12-05 23:27:57 +0900

    20231205

--- src/Solution.java
+++ src/Solution.java
@@ -1,20 +1,25 @@
-import java.util.Stack;
-
-class Solution {
-    boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == ')') {
-                if (stack.isEmpty()) {
-                    return false;
-                } else {
-                    stack.pop();
-                }
-            } else {
-                stack.push(c);
+public class Solution {
+    public int solution(int[] stones, int k) {
+        int answer = 0;
+        for (int i = 0; i < stones.length; i++) {
+            int maxNumber = 0;
+            for (int j = 0; j < k && i + j < stones.length; j++) {
+                maxNumber = Math.max(maxNumber, stones[i + j]);
+                System.out.println("j = " + j);
             }
+            if (maxNumber == 0) {
+                break;
+            }
+            answer = Math.min(answer, maxNumber);
         }
-        return stack.isEmpty();
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] stones = {2, 4, 5, 3, 2, 1, 4, 2, 5, 1};
+        int k = 3;
+        int answer = s.solution(stones, k);
+        System.out.println("answer = " + answer);
     }
 }
